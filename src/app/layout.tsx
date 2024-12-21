113bddc4abe53b11720cf6bb065df543f2f38220
mhanna <mandzyuk.annie@gmail.com>
2024-12-21 02:08:24 +0100

    font fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'locomotive-scroll/dist/locomotive-scroll.css';
 import SmoothScroll from './components/smooth-scroll';
